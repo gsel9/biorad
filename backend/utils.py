ea9fcc4b5ecf0a3a1650693c9ca44efbe12cb696
SeverinLangberg <Langberg91@gmail.com>
2018-12-22 12:53:51 +0100

    clean up train fit predict pipe

--- backend/utils.py
+++ backend/utils.py
@@ -32,65 +32,77 @@ class BootstrapOutOfBag:
         self.random_state = random_state
 
     def split(self, X, y, **kwargs):
-        """Generates Out-of-Bag (OOB) samples.
+        """Generates Out-of-Bag samples.
 
         Args:
             X (array-like): The predictor data.
             y (array-like): The target data.
 
         Returns:
-            (genrator): An iterable with OOB samples.
+            (genrator): An iterable with X and y sample indicators.
 
         """
-
-        rand_gen = np.random.RandomState(self.random_state)
+        rgen = np.random.RandomState(self.random_state)
 
         nrows, _ = np.shape(X)
         sample_indicators = np.arange(nrows)
         for _ in range(self.n_splits):
-            train_idx = rand_gen.choice(
+            # Sample with replacement.
+            train_idx = rgen.choice(
                 sample_indicators, size=nrows, replace=True
             )
+            # Oberervations not part of training set defines test set.
             test_idx = np.array(
                 list(set(sample_indicators) - set(train_idx)), dtype=int
             )
             yield train_idx, test_idx
 
 
-def listdir(path_to_dir, skip_tail=('.csv'), skip_head=('.')):
+def check_support(support):
+    # Model comparison auxillary function formatting selected support.
+    if np.ndim(support) > 1:
+        return np.squeeze(support)
 
-    labels = []
-    for label in os.listdir(path_to_dir):
-        if not label.endswith(skip_tail) and not label.startswith(skip_head):
-            labels.append(str(label))
-
-    return labels
+    if not isinstance(support, np.ndarray):
+        return np.array(support, dtype=int)
 
 
 def train_test_z_scores(X_train, X_test):
-    """Compute Z-scores for training and test sets.
+    """Apply Z-score transformation to features in training and test sets.
 
     Args:
         X_train (array-like): Training set.
         X_test (array-like): Test set.
 
     Returns:
-        (tuple): Standard score values for training and test set.
+        (tuple): Transformed training and test set.
 
     """
-
-    # NOTE: Avoid information leakage and make train/test sets comparable.
     scaler = StandardScaler()
     X_train_std = scaler.fit_transform(X_train)
+    # Apply training params in transforming test set: renders test set
+    # comparable to training set while preventing information bleeding.
     X_test_std = scaler.transform(X_test)
 
     return X_train_std, X_test_std
 
 
-def scale_fit_predict632(*args, score_func=None, **kwargs):
+def scale_fit_predict632(model, X_train, X_test, y_train, y_test, score_func):
+    """Assess model performance on Z-score transformed training and test sets.
 
-    model, X_train, X_test, y_train, y_test = args
+    Args:
+        model (object): An untrained model with `fit()` and `predict` methods.
+        X_train (array-like): Training set.
+        X_test (array-like): Test set.
+        y_train (array-like): Training set ground truths.
+        y_test (array-like): Test set ground truths.
+        score_func (function): A score function for model performance
+            evaluation.
 
+    Returns:
+        (float):
+
+    """
     # Compute Z scores.
     X_train_std, X_test_std = train_test_z_scores(X_train, X_test)
 
@@ -98,12 +110,12 @@ def scale_fit_predict632(*args, score_func=None, **kwargs):
 
     # Aggregate model predictions.
     y_train_pred = model.predict(X_train_std)
-    train_score = score_func(y_train, y_train_pred)
-
     y_test_pred = model.predict(X_test_std)
+
+    train_score = score_func(y_train, y_train_pred)
     test_score = score_func(y_test, y_test_pred)
 
-    # Compute train and test .632+ scores.
+    # Compute .632+ scores.
     train_632_score = point632plus_score(
         y_train, y_train_pred, train_score, test_score
     )
@@ -113,61 +125,41 @@ def scale_fit_predict632(*args, score_func=None, **kwargs):
     return train_632_score, test_632_score
 
 
-
-def check_support(support):
-
-    if np.ndim(support) > 1:
-        return np.squeeze(support)
-
-    if not isinstance(support, np.ndarray):
-        return np.array(support, dtype=int)
-
-
 def point632plus_score(y_true, y_pred, train_score, test_score):
-    """Compute .632+ score
+    """Compute .632+ score for binary classification.
+
+    Args:
+        y_true (array-like): Ground truths.
+        y_pred (array-like): Predictions.
+        train_score (float): Resubstitution score.
+        test_score (float): True score.
+
+    Returns:
+        (float): The .632+ score value.
+
     """
-
-    gamma = _no_info_rate(y_true, y_pred)
-    # To account for gamma <= train_score/train_score < gamma <= test_score
-    # in which case R can fall outside of [0, 1].
+    gamma = _no_info_rate_binary(y_true, y_pred)
+    # Calculate adjusted parameters as described in Efron & Tibshiranir paper.
     test_score_marked = min(test_score, gamma)
+    r_marked = relative_overfit_rate(train_score, test_score, gamma)
 
-    # Adjusted relative overfit rate.
-    r_marked = _relative_overfit_rate(train_score, test_score, gamma)
-
-    # Compute .632+ train score.
-    return _point632plus(train_score, test_score, r_marked, test_score_marked)
-
-
-def _no_info_rate(y_true, y_pred):
-    # NB: Only applicable to a dichotomous classification problem.
-    p_one = np.sum(y_true) / np.size(y_true)
-    q_one = np.sum(y_pred) / np.size(y_pred)
-
-    return p_one * (1 - q_one) + (1 - p_one) * q_one
-
-
-@jit
-def _point632plus(train_score, test_score, r_marked, test_score_marked):
-    # Paper implementation of .632+ score.
-    point632 = 0.368 * train_score + 0.632 * test_score
-    frac = (0.368 * 0.632 * r_marked) / (1 - 0.368 * r_marked)
-
-    return point632 + (test_score_marked - train_score) * frac
-
-
-@jit
-def _relative_overfit_rate(train_score, test_score, gamma):
-    # Paper implementation of relative overfitting rate.
-    if test_score > train_score and gamma > train_score:
-        return (test_score - train_score) / (gamma - train_score)
-    else:
-        return 0
+    return point632plus(train_score, test_score, r_marked, test_score_marked)
 
 
 @jit
 def point632plus(train_score, test_score, r_marked, test_score_marked):
+    """Calculate the .632+ score from parameters.
 
+    Args:
+        train_score (float): The resubstitution score.
+        test_score (float): The true score.
+        r_marked (float): Adjusted relative overfitting rate.
+        test_score_marked (float):
+
+    Returns:
+        (float): The .632+ score value.
+
+    """
     point632 = 0.368 * train_score + 0.632 * test_score
     frac = (0.368 * 0.632 * r_marked) / (1 - 0.368 * r_marked)
 
@@ -176,32 +168,26 @@ def point632plus(train_score, test_score, r_marked, test_score_marked):
 
 @jit
 def relative_overfit_rate(train_score, test_score, gamma):
+    """Calculate the relative overfitting rate from parameters.
 
+    Args:
+        train_score (float): The resubstitution score.
+        test_score (float): The true score.
+        gamma (float): The no information rate.
+
+    Returns:
+        (float): The relative overfitting rate value.
+
+    """
     if test_score > train_score and gamma > train_score:
         return (test_score - train_score) / (gamma - train_score)
     else:
         return 0
 
 
-# NOTE: Need only use sum(y_) if y_ is binary.
-def no_info_rate(y_true, y_pred):
-
+def _no_info_rate_binary(y_true, y_pred):
     # NB: Only applicable to a dichotomous classification problem.
-    p_one = np.sum(y_true == 1) / np.size(y_true)
-    q_one = np.sum(y_pred == 1) / np.size(y_pred)
+    p_one = np.sum(y_true) / np.size(y_true)
+    q_one = np.sum(y_pred) / np.size(y_pred)
 
     return p_one * (1 - q_one) + (1 - p_one) * q_one
-
-
-def point632plus_score(y_true, y_pred, train_score, test_score):
-
-    gamma = no_info_rate(y_true, y_pred)
-    # To account for gamma <= train_score/train_score < gamma <= test_score
-    # in which case R can fall outside of [0, 1].
-    test_score_marked = min(test_score, gamma)
-
-    # Adjusted R.
-    r_marked = relative_overfit_rate(train_score, test_score, gamma)
-
-    # Compute .632+ train score.
-    return point632plus(train_score, test_score, r_marked, test_score_marked)
