771a0d94ee72c41db32cd4cc92681220670d830f
SeverinLangberg <Langberg91@gmail.com>
2019-01-04 09:39:59 +0100

    TEMP: storing updated API and func arg code for model selection

--- model_comparison/tests/test_model_selection.py
+++ model_comparison/tests/test_model_selection.py
@@ -53,17 +53,18 @@ def data():
 
 if __name__ == '__main__':
     # Demo run:
+    # * 97.80 % accuracy seems to be a fairly good score.
+    #
+    # TODO: Need seed + clf + selector name for unique ID to prelim results files.
 
     from sklearn.datasets import load_breast_cancer
-
+    from sklearn.model_selection import train_test_split
     from sklearn.pipeline import Pipeline
     from sklearn.metrics import roc_auc_score
     from sklearn.preprocessing import StandardScaler
-    from sklearn.model_selection import train_test_split
 
-    from sklearn.feature_selection.univariate_selection import chi2
-    from sklearn.feature_selection.univariate_selection import SelectPercentile
-
+    # TEMP:
+    from sklearn.feature_selection.univariate_selection import SelectPercentile, chi2
     from sklearn.ensemble import RandomForestClassifier
 
     X, y = load_breast_cancer(return_X_y=True)
@@ -76,24 +77,34 @@ if __name__ == '__main__':
         ('clf', RandomForestClassifier(random_state=0))
     ])
 
+    # Can specify hparam distr in config files that acn direclty be read into
+    # Python dict with hyperopt distirbutions?
+
     # Parameter search space
     space = {}
-    space['clf__max_leaf_nodes'] = scope.int(
-        hp.quniform('clf__max_leaf_nodes', 30, 150, 1)
-    )
-    space['clf__min_samples_leaf'] = scope.int(
-        hp.quniform('clf__min_samples_leaf', 20, 500, 5)
-    )
-    space['clf__class_weight'] = hp.choice('clf__class_weight', [None,])
+    # Random number between 50 and 100
     space['kbest__percentile'] = hp.uniform('kbest__percentile', 50, 100)
+    # Random number between 0 and 1
+    #space['clf__l1_ratio'] = hp.uniform('clf__l1_ratio', 0.0, 1.0)
+    # Log-uniform between 1e-9 and 1e-4
+    #space['clf__alpha'] = hp.loguniform('clf__alpha', -9*np.log(10), -4*np.log(10))
+    # Random integer in 20:5:80
+    #space['clf__n_iter'] = 20 + 5 * hp.randint('clf__n_iter', 12)
+    # Random number between 50 and 100
+    space['clf__class_weight'] = hp.choice('clf__class_weight', [None,]) #'balanced']),
+    # Discrete uniform distribution
+    space['clf__max_leaf_nodes'] = scope.int(hp.quniform('clf__max_leaf_nodes', 30, 150, 1))
+    # Discrete uniform distribution
+    space['clf__min_samples_leaf'] = scope.int(hp.quniform('clf__min_samples_leaf', 20, 500, 5))
 
-    results = model_selection(
+    results = bbc_cv_selection(
         X_train, y_train,
         algo=tpe.suggest,
+        model_id='',
         model=pipe,
-        space=space,
+        param_space=space,
         score_func=roc_auc_score,
-        path_tmp_results='',
+        path_tmp_results='./',
         cv=5,
         oob=5,
         max_evals=7,
