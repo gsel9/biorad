6caf6eaa2f7ab9bd3d338ea9400b31e3fe2db3a1
SeverinLangberg <Langberg91@gmail.com>
2019-01-04 15:48:38 +0100

    WIP: clean up

--- experiments/utils/selectors.py
+++ experiments/utils/selectors.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# feature_selection.py
+# selectors.py
 #
 
 """
 Wrapped feature selection algorithms providing API compatible with model
-comparison framework.
+comparison framework and scikti-learn Pipeline objects.
 """
 
 __author__ = 'Severin Langberg'
@@ -16,8 +16,6 @@ import mifs
 
 import numpy as np
 import pandas as pd
-
-import utils
 
 from scipy import stats
 from ReliefF import ReliefF
@@ -32,19 +30,16 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 class BaseSelector:
 
-    def __init__(self, z_scoring, error_handling='return_all'):
+    def __init__(self, error_handling='return_all'):
 
-        self.z_scoring = z_scoring
         self.error_handling = error_handling
 
-        # Apply Z-score transformation to data.
-        if self.z_scoring:
-            self._scaler = StandardScaler()
-        else:
-            self._scaler = None
+        self.X = None
+        self.y = None
 
     @property
     def support(self):
+        """Returns numpy.ndarray of feature support indicators."""
 
         if self._support is None:
             return
@@ -56,6 +51,61 @@ class BaseSelector:
     def _check_X_y(X, y):
 
         return X, y
+
+    @staticmethod
+    def check_subset(X):
+        """Formatting and type checking of feature subset.
+
+        Args:
+            X (array-like):
+
+        Returns:
+            (array-like): Formatted feature subset.
+
+        """
+
+        # Check training set.
+        if np.ndim(X) > 2:
+            if np.ndim(np.squeeze(X)) > 2:
+                raise RuntimeError('X train ndim {}'.format(np.ndim(X)))
+            else:
+                X = np.squeeze(X)
+        if np.ndim(X) < 2:
+            if np.ndim(X.reshape(-1, 1)) == 2:
+                X = X.reshape(-1, 1)
+            else:
+                raise RuntimeError('X train ndim {}'.format(np.ndim(X_train)))
+
+        return np.array(X, dtype=float)
+
+    @staticmethod
+    def check_support(support, X):
+        """Formatting of feature subset indicators.
+
+        Args:
+            support (array-like): Feature subset indicators.
+            X (array-like): Original feature matrix.
+
+        Returns:
+            (array-like): Formatted indicators of feature subset.
+
+        """
+        if not isinstance(support, np.ndarray):
+            support = np.array(support, dtype=int)
+
+        # NB: Include all features if none were selected.
+        if len(support) - 1 < 1:
+            support = np.arange(X.shape[1], dtype=int)
+        else:
+            if np.ndim(support) > 1:
+                support = np.squeeze(support)
+            if np.ndim(support) < 1:
+                support = support[np.newaxis]
+            if np.ndim(support) != 1:
+                raise RuntimeError(
+                    'Invalid dimension {} to support.'.format(np.ndim(support))
+                )
+        return support
 
 
 class PermutationSelection(BaseSelector, BaseEstimator, TransformerMixin):
@@ -77,19 +127,17 @@ class PermutationSelection(BaseSelector, BaseEstimator, TransformerMixin):
         test_size,
         model,
         hparams=None,
-        z_scoring=True,
         error_handling='return_all',
         random_state=None,
     ):
 
-        super().__init__(z_scoring, error_handling)
+        super().__init__(error_handling)
 
         self.score_func = score_func
         self.num_rounds = num_rounds
         self.test_size = test_size
         self.model = model
         self.hparams = hparams
-        self.z_scoring = z_scoring
         self.random_state = random_state
 
         self.rgen = np.random.RandomState(self.random_state)
@@ -110,11 +158,6 @@ class PermutationSelection(BaseSelector, BaseEstimator, TransformerMixin):
             test_size=self.test_size,
             random_state=self.random_state
         )
-        # Perform Z-score transformation.
-        if self.z_scoring and self._scaler is not None:
-            X_train = self._scaler.fit_transform(X_train)
-            X_test = self._scaler.transform(X_test)
-
         # NB: Error handling.
         try:
             self.model.fit(X_train, y_train)
@@ -123,15 +166,13 @@ class PermutationSelection(BaseSelector, BaseEstimator, TransformerMixin):
 
         avg_imp = self.feature_permutation_importance(X_test, y_test)
         # Return features contributing to model performance as support.
-        _support = np.where(avg_imp > 0)
-        # Formatting and error handling.
-        self.support = utils.formatting.check_support(_support, self.X)
+        self.support = self.check_support(np.where(avg_imp > 0), self.X)
 
         return self
 
     def transform(self):
 
-        return self.support.formatting.check_subset(self.X[:, self.support])
+        return self.check_subset(self.X[:, self.support])
 
     def _feature_permutation_importance(self, X, y):
         # Returns average feature permutation importance.
@@ -167,35 +208,28 @@ class WilcoxonSelection(BaseSelector, BaseEstimator, TransformerMixin):
 
     def __init__(
         self,
-        z_scoring,
         thresh=0.05,
         bf_correction=True,
         error_handling='return_all'
     ):
 
-        super().__init__(z_scoring, error_handling)
+        super().__init__(error_handling)
 
         self.thresh = thresh
-        self.z_scoring = z_scoring
         self.bf_correction = bf_correction
 
     def fit(self, X, y=None):
 
         self.X, self.y = self._check_X_y(X, y)
 
-        # Perform Z-score transformation.
-        if self.z_scoring and self._scaler is not None:
-            self.X = self._scaler.fit_transform(self.X)
-
-        _support = self.wilcoxon_signed_rank()
         # Formatting and error handling.
-        self.support = utils.formatting.check_support(_support, self.X)
+        self.support = self.check_support(self.wilcoxon_signed_rank(), self.X)
 
         return self
 
     def transform(self):
 
-        return self.support.formatting.check_subset(self.X[:, self.support])
+        return self.check_subset(self.X[:, self.support])
 
     def wilcoxon_signed_rank(self):
         """The Wilcoxon signed-rank test to determine if two dependent samples were
@@ -237,15 +271,13 @@ class ReliefFSelection(BaseSelector, BaseEstimator, TransformerMixin):
 
     def __init__(
         self,
-        z_scoring,
         num_neighbors,
         num_features,
         error_handling='return_all'
     ):
 
-        super().__init__(z_scoring, error_handling)
+        super().__init__(error_handling)
 
-        self.z_scoring = z_scoring
         self.num_neighbors = num_neighbors
         self.num_features = num_features
 
@@ -253,24 +285,23 @@ class ReliefFSelection(BaseSelector, BaseEstimator, TransformerMixin):
 
         self.X, self.y = self._check_X_y(X, y)
 
-        # Perform Z-score transformation.
-        if self.z_scoring and self._scaler is not None:
-            self.X = self._scaler.fit_transform(self.X)
-
         selector = ReliefF(n_neighbors=self.num_neighbors)
         selector.fit(self.X, self.y)
 
         _support = selector.top_features[:self.num_features]
 
-        self.support = utils.formatting.check_support(_support, self.X)
+        self.support = self.check_support(_support, self.X)
 
         return self
 
     def transform(self):
 
-        return self.support.formatting.check_subset(self.X[:, self.support])
+        return self.check_subset(self.X[:, self.support])
 
 
+# NOTE:
+# * Cloned from: https://github.com/danielhomola/mifs
+# * Requirements: TODO
 class MRMRSelection(BaseSelector, BaseEstimator, TransformerMixin):
     """Perform feature selection with the minimum redundancy maximum relevancy
     algortihm.
@@ -283,10 +314,6 @@ class MRMRSelection(BaseSelector, BaseEstimator, TransformerMixin):
 
     """
 
-    # NOTE:
-    # * Cloned from: https://github.com/danielhomola/mifs
-    # * Requirements: TODO
-
     def __init__(
         self,
         z_scoring,
@@ -295,19 +322,14 @@ class MRMRSelection(BaseSelector, BaseEstimator, TransformerMixin):
         error_handling='return_all'
     ):
 
-        super().__init__(z_scoring, error_handling)
+        super().__init__(error_handling)
 
-        self.z_scoring = z_scoring
         self.k = k
         self.num_features = num_features
 
     def fit(self, X, y=None):
 
         self.X, self.y = self._check_X_y(X, y)
-
-        # Perform Z-score transformation.
-        if self.z_scoring and self._scaler is not None:
-            self.X = self._scaler.fit_transform(self.X)
 
         # If 'auto': n_features is determined based on the amount of mutual
         # information the previously selected features share with y.
@@ -317,14 +339,18 @@ class MRMRSelection(BaseSelector, BaseEstimator, TransformerMixin):
             n_features=self.num_features,
             categorical=True,
         )
-        selector.fit(X_train_std, y_train)
+        selector.fit(self.X, self.y)
 
-        _support = np.where(selector.support_ == True)
-
-        self.support = utils.formatting.check_support(_support, self.X)
-
+        self.support = self.check_support(
+            np.where(selector.support_ == True), self.X
+        )
         return self
 
     def transform(self):
 
-        return self.support.formatting.check_subset(self.X[:, self.support])
+        return self.check_subset(self.X[:, self.support])
+
+
+if __name__ == '__main__':
+
+    pass
