a3c485800711a63c5d2be51bb4bb194bd6835ae3
SeverinLangberg <Langberg91@gmail.com>
2019-01-07 07:44:40 +0100

    WIP: some functinoal tests to pipelines

--- experiment/tests/test_pipelines.py
+++ experiment/tests/test_pipelines.py
@@ -1,52 +1,75 @@
-if __name__ == '__main__':
-    # ToDos:
-    # * Make Pipeline encompassing feature selection and classifcaiton.
-    #   Remember to include StandardScaler at appropriate locations.
-    # *
-    # * Determine reasonable distributions for hparams (figure out how
-    #   distribution boundaries are determined by sklearn).
-    # *
-    # TODO: Move to backend?
-    import sys
-    sys.path.append('./../../model_comparison')
+import sys
+sys.path.append('./..')
+sys.path.append('./../../model_comparison')
 
-    import os
-    import backend
-    import model_selection
-    import comparison_frame
+import os
+import pytest
+import backend
+import model_selection
+import comparison_frame
 
-    from selector_configs import selectors
-    from estimator_configs import classifiers
+from selector_configs import selectors
+from estimator_configs import classifiers
 
-    from sklearn.datasets import mnist
+from sklearn.datasets import load_breast_cancer
 
-    X =
-    # Demo run:
-    # * 97.80 % accuracy seems to be a fairly good score.
-    #
-    # TODO: Need seed + clf + selector name for unique ID to prelim results files.
-    import model_selection
+from sklearn.model_selection import train_test_split
 
-    from hyperopt import hp
-    from hyperopt import tpe
-    from hyperopt.pyll.base import scope
 
-    from sklearn.datasets import load_breast_cancer
-    from sklearn.model_selection import train_test_split
-    from sklearn.pipeline import Pipeline
-    from sklearn.metrics import roc_auc_score
-    from sklearn.preprocessing import StandardScaler
+SEED = 0
 
-    # TEMP:
-    from sklearn.feature_selection.univariate_selection import SelectPercentile, chi2
-    from sklearn.ensemble import RandomForestClassifier
+
+@pytest.fixture
+def data(test_size=0.2):
+
+    global SEED
 
     X, y = load_breast_cancer(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=0
+        X, y, test_size=test_size, random_state=SEED
+    )
+    return X_train, X_test, y_train, y_test
+
+
+@pytest.fixture
+def pipes_and_params():
+
+    return backend.formatting.pipelines_from_configs(
+        selectors, classifiers
+    )
+
+
+def test_pipe_format(pipes_and_params):
+
+    for label, (pipe, params) in pipes_and_params.items():
+        pass
+
+def test_pipe_fit(data):
+
+    X_train, _, y_train, _ = data
+
+    for label, (pipe, params) in pipes_and_params.items():
+        pipe.fit(X_train, y_train)
+
+
+def test_pipe_fit_predict(data):
+
+    X_train, X_train, y_train, y_test = data
+
+    for label, (pipe, params) in pipes_and_params.items():
+        pipe.fit(X_train, y_train)
+        pipe.predict(X_test)
+
+
+if __name__ == '__main__':
+
+    X, y = load_breast_cancer(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=SEED
     )
     pipes_and_params = backend.formatting.pipelines_from_configs(
         selectors, classifiers
     )
-    for pipe_label, estimator_items in pipes_and_params.items():
-        pass
+    for label, (pipe, params) in pipes_and_params.items():
+        pipe.fit(X_train, y_train)
+        pipe.predict(X_test)
