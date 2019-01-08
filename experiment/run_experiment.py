d8f7943223a04cd1f560a3d7b96f8fd1a66f9c86
SeverinLangberg <Langberg91@gmail.com>
2019-01-08 12:12:16 +0100

    WIP: ini run w/radiomics data

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -1,59 +1,15 @@
 # -*- coding: utf-8 -*-
 #
-# baseline.py
+# run_experiment.py
 #
 
 """
-Configurations for radiomics baseline experiments.
+Experimental setup.
 
-Goal: Specify classifiers, FS algorithms and experimental setup (run configs,
-e.g. random states) and execute.
-
-To Dos:
-* Create module (YAML file) with classifiers and FS algorithms that can be can
-  be sewn together in sklearn pipelines passed as a dict with unique labels.
-* Include a flag in each FS class whether or not should standardize data in
-  advance.
-
-Notes:
-* Need something that cant raverse a module and collect specified objects.
-
-# Instantiate selector representation for pipeline compatibility.
-#selector = feature_selection.Selector(selector_id, procedure)
-# Combine parameter grids.
-#param_grid = [
-#    _format_hparams(selector_params[selector_id]),
-#    _format_hparams(estimator_params[estimator_id])
-#]
-# Create estiamtor pipeline.
-#pipeline = Pipeline([
-#    (SELECTOR_ID, selector(random_state=random_state)),
-#    (ESTIMATOR_ID, estimator(random_state=random_state))
-#])
-
-
-def _format_hparams(params, kind='estimator'):
-    # Format parameter names to scikit Pipeline compatibility.
-
-    global ESTIMATOR_ID, SELECTOR_ID
-
-    if kind == 'estimator':
-        ext = ESTIMATOR_ID
-    elif kind == 'selector':
-        ext = SELECTOR_ID
-    else:
-        raise ValueError('Invalid kind {} not in (`estimator`, `selector`)'
-                         ''.format(kind))
-    # Update keys.
-    for set_key,  param_set in hparams.items():
-        hparams[set_key] = {
-            key.replace(key, ('__').join((ext, key)): params
-            for key, params in param_set.items()
-        }
-    return hparams
+Notes
+* Specify the number of original features in the data set in config.
 
 """
-
 
 import sys
 import time
@@ -82,27 +38,25 @@ def load_predictors(path_to_data, index_col=0, regex=None):
 
 
 if __name__ == '__main__':
-    # TODO: Move to backend?
     import sys
     sys.path.append('./../model_comparison')
 
     import os
     import backend
+    import comparison
     import model_selection
-    import comparison_frame
 
-    from selector_configs import selectors
-    from estimator_configs import classifiers
+    from configs.selector_configs import selectors
+    from configs.estimator_configs import classifiers
 
     from hyperopt import tpe
 
     from sklearn.metrics import roc_auc_score
-    from sklearn.metrics import matthews_corrcoef
-    from sklearn.metrics import precision_recall_fscore_support
+    #from sklearn.metrics import matthews_corrcoef
+    #from sklearn.metrics import precision_recall_fscore_support
 
     # TEMP:
     from sklearn.datasets import load_breast_cancer
-    from sklearn.preprocessing import StandardScaler
 
     X, y = load_breast_cancer(return_X_y=True)
 
@@ -114,12 +68,12 @@ if __name__ == '__main__':
     #y = load_target('./../../data_source/to_analysis/target_dfs.csv')
 
     # RESULTS LOCATION:
-    #path_to_results = './../../data_source/experiments/no_filtering_dfs.csv'
+    #path_to_results = './../data/experiments/test_results.csv'
 
     # SETUP:
-    CV = 10
-    OOB = 500
-    MAX_EVALS = 100
+    CV = 3
+    OOB = 5
+    MAX_EVALS = 4
     SCORING = roc_auc_score
 
     # Generate seeds for pseudo-random generators to use in each experiment.
@@ -130,8 +84,11 @@ if __name__ == '__main__':
     pipes_and_params = backend.formatting.pipelines_from_configs(
         selectors, classifiers
     )
+    # QUESTION: How to easily config and execute experiments?
+    # * Maintain config setup for each experiment as a reminder on the setup.
+    # *
     """
-    comparison_frame.model_comparison(
+    comparison.model_comparison(
         model_selection.bbc_cv_selection,
         X, y,
         tpe.suggest,
@@ -141,16 +98,13 @@ if __name__ == '__main__':
         OOB,
         MAX_EVALS,
         shuffle=True,
-        verbose=1,
+        verbose=0,
         random_states=random_states,
         alpha=0.05,
         balancing=True,
         write_prelim=True,
         error_score=np.nan,
         n_jobs=-1,
-        path_final_results='./test_results'
+        path_final_results='./../data/test_results.csv'
     )
-    pipe, params = pipes_and_params['PermutationSelectionRF_PLSRegression']
-    #print(params)
     """
-    print(type(pipes_and_params))
