72ad256257352bf8e29eea773e4be57f44ce1f16
SeverinLangberg <Langberg91@gmail.com>
2019-01-09 16:23:47 +0100

    setup time complexity benchmark experiment

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -50,6 +50,9 @@ def load_predictors(path_to_data, index_col=0, regex=None):
 
 
 if __name__ == '__main__':
+    # EXPERIMENT: Timing the execution of the assumed most demanding experiment
+    # to estimate an upper bound on experimental time complexity.
+    #
     import sys
     sys.path.append('./../model_comparison')
 
@@ -65,25 +68,24 @@ if __name__ == '__main__':
     from hyperopt import tpe
 
     from sklearn.metrics import roc_auc_score
-    #from sklearn.metrics import matthews_corrcoef
-    #from sklearn.metrics import precision_recall_fscore_support
 
     # FEATURE SET:
     X = load_predictors('./../../data_source/to_analysis/complete_decorr.csv')
 
     # TARGET:
-    y = load_target('./../../data_source/to_analysis/target_lrr.csv')
-    #y = load_target('./../../data_source/to_analysis/target_dfs.csv')
+    #y = load_target('./../../data_source/to_analysis/target_lrr.csv')
+    y = load_target('./../../data_source/to_analysis/target_dfs.csv')
 
     # RESULTS LOCATION:
-    path_to_results = './../data/experiments/complete_decorr_dfs.csv'
+    path_to_results = './../data/experiments/bigo_timing_dfs.csv'
+    #path_to_results = './../data/experiments/complete_decorr_dfs.csv'
     #path_to_results = './../data/experiments/complete_decorr_lrr.csv'
 
-    # SETUP:
+    # EXPERIMENTAL SETUP:
     CV = 2#10
-    OOB = 2#300
-    MAX_EVALS = 2#100
-    NUM_EXP_REPS = 2#30
+    OOB = 3#00
+    MAX_EVALS = 3#100
+    NUM_EXP_REPS = 1 #30
     SCORING = roc_auc_score
 
     # Generate seeds for pseudo-random generators to use in each experiment.
@@ -94,11 +96,14 @@ if __name__ == '__main__':
     pipes_and_params = backend.formatting.pipelines_from_configs(
         selectors, classifiers
     )
+    # TMP: The assumed most demanding experiment
+    name = 'PermutationSelection_SVC'
+    _pipes_and_params = {name: pipes_and_params[name]}
     comparison.model_comparison(
         model_selection.bbc_cv_selection,
         X, y,
         tpe.suggest,
-        pipes_and_params,
+        _pipes_and_params,
         SCORING,
         CV,
         OOB,
