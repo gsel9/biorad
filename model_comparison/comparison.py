395882d9a8d1237c377b35fe0877869778e03201
SeverinLangberg <Langberg91@gmail.com>
2019-03-04 09:20:59 +0100

    clean up comparison module

--- model_comparison/comparison.py
+++ model_comparison/comparison.py
@@ -10,25 +10,11 @@ Framework for performing model comparison experiments.
 __author__ = 'Severin Langberg'
 __email__ = 'langberg91@gmail.com'
 
-import numpy as np
-import pandas as pd
-
-from utils import ioutil
-from collections import OrderedDict
-
-from sklearn.externals import joblib
-from sklearn.pipeline import Pipeline
-
-from joblib import Memory
-from shutil import rmtree
-from tempfile import mkdtemp
 from multiprocessing import cpu_count
-
 from smac.configspace import ConfigurationSpace
-
-
-# Name of directory to store temporary results.
-TMP_RESULTS_DIR = 'tmp_model_comparison'
+from sklearn.pipeline import Pipeline
+from sklearn.externals import joblib
+from utils import ioutil
 
 
 def model_comparison(
@@ -36,47 +22,32 @@ def model_comparison(
     X, y,
     experiments,
     score_func,
-    cv,
-    max_evals,
-    shuffle=True,
-    verbose=0,
+    cv: int,
+    max_evals: int,
+    shuffle: bool=True,
+    verbose: int=0,
     random_states=None,
-    output_dir=None,
-    write_prelim=False,
-    n_jobs=1,
-    path_final_results=None
+    output_dir: str=None,
+    write_prelim: bool=False,
+    n_jobs: int=1,
+    path_final_results: str=None
 ):
     """
     Compare model performances with optional feature selection.
 
     Args:
-        comparison_scheme (function):
-        X (array-like):
-        y (array-like):
-        algo ():
-        cv (int):
-        max_evals (int):
-        shuffle ():
-        verbose ():
-        random_states (array-like):
-        balancing ():
-        write_prelim (bool):
-        error_score (float):
-        n_jobs (int):
-        path_final_results (str):
 
     """
-    global TMP_RESULTS_DIR
-
     # Setup temporary directory to store preliminary results.
     if write_prelim:
-        path_tmp_results = ioutil.setup_tempdir(TMP_RESULTS_DIR, root='.')
+        path_tmp_results = ioutil.setup_tempdir(
+            'tmp_model_comparison', root='.'
+        )
     else:
         path_tmp_results = None
-
     # Set the default number of available working CPUs.
     if n_jobs is None:
-        n_jobs = cpu_count() - 1 if cpu_count() > 1 else cpu_count()
+        n_jobs = cpu_count() - 1 if cpu_count() > 1 else 1
 
     results = []
     for experiment_id, setup in experiments.items():
@@ -97,8 +68,7 @@ def model_comparison(
                 for random_state in random_states
             )
         )
-
-    # Tear down temporary dirs after saving final results to disk.
+    # Remove temporary dir if succesfully writing final results to disk.
     if write_prelim:
         _cleanup_prelim(path_tmp_results)
 
@@ -108,13 +78,17 @@ def model_comparison(
 
 
 def config_experiment(setup, random_state):
-    """
+    """Setup experimental configurations:
+    - Joins hyperparameter spaces.
+    - Assigns random state to stichastic algorithms.
+    - Formats workflow as a scikit-learn Pipeline object.
+
+    Returns:
+        (tuple): Pipeline and hyperparameter space.
 
     """
-
     config_space = ConfigurationSpace()
     config_space.seed(random_state)
-
     for name, algorithm in setup:
         # Join hyperparameter spaces.
         if hasattr(algorithm, 'config_space'):
@@ -131,7 +105,6 @@ def config_experiment(setup, random_state):
 
 
 def _write_results(path_final_results, results):
-
     # Write final results to disk.
     ioutil.write_final_results(path_final_results, results)
 
@@ -139,7 +112,6 @@ def _write_results(path_final_results, results):
 
 
 def _cleanup_prelim(path_tmp_results):
-
     # Remove temporary directory if process completed succesfully.
     ioutil.teardown_tempdir(path_tmp_results)
 
