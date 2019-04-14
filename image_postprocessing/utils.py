4ca1a3026bfba64ef9a98c4d0c3d7b54efacca1a
SeverinLangberg <Langberg91@gmail.com>
2019-04-14 20:53:54 +0200

    checking up matlab convertes

--- image_postprocessing/utils.py
+++ image_postprocessing/utils.py
@@ -74,7 +74,7 @@ def nrrd_to_ndarray(path_to_file, return_header=False):
         return np.array(data, dtype=float)
 
 
-def show_ct_stack(stack, slice_dim=0):
+def show_stack(stack, slice_dim=0):
     """
 
     Notes:
@@ -160,14 +160,9 @@ def img_dump(stack, dir: Path, base_name: str, slice_dim=0):
 if __name__ == '__main__':
     # Demo run.
     path_to_stack = './../../data_source/images/ct_nrrd/P008CT.nrrd'
-    path_to_mask = './../../data_source/images/masks_nrrd/P008mask.nrrd'
 
     image = nrrd_to_ndarray(path_to_stack)
-    mask = nrrd_to_ndarray(path_to_mask)
-
-    #cropped_image = image * mask
 
     plt.gray()
-
     show_stack(image)
     #img_dump(img_res, Path('./../lung_data/img'), 'stack_img', slice_dim=0)
